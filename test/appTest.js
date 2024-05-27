9711e78ae374640e1771ec656e8b9d8c65e15d54
naopeke <naopeke@protonmail.com>
2024-05-27 12:15:10 +0200

    initial commit

---
+++ test/appTest.js
@@ -0,0 +1,12 @@
+import { assert } from 'chai';
+import { likes } from '../index.js';
+
+describe('example tests', function() {
+    it('should return correct text', function() {
+        assert.strictEqual(likes([]), 'no one likes this');
+        assert.strictEqual(likes(['Peter']), 'Peter likes this');
+        assert.strictEqual(likes(['Jacob', 'Alex']), 'Jacob and Alex like this');
+        assert.strictEqual(likes(['Max', 'John', 'Mark']), 'Max, John and Mark like this');
+        assert.strictEqual(likes(['Alex', 'Jacob', 'Mark', 'Max']), 'Alex, Jacob and 2 others like this');
+    });
+});
